fef1249dcbe52bd1c49520293ea7f7a660b5d306
Gerardo Fernandez <gerardojose13@gmail.com>
2019-01-03 01:47:07 -0800

    adds additional select tags

--- src/components/Form.js
+++ src/components/Form.js
@@ -39,12 +39,21 @@ class Form extends Component{
         school: "",
         year: "",
         level: "",
-        data: [],
+        data: "",
+        collegeName: "",
+        image: "",
+        programInterest: [],
+        rnLicense: [],
+        leadStatus: [],
+        compAccess: [],
+        usCitizen: [],
+        isLoaded: false,
+        error: null,
       }
 
-    //   componentDidMount(){
-    //       this.handleRender();
-    //   }
+      componentDidMount(){
+          this.handleRender();
+      }
     
       handleChange = event => {
         this.setState({
@@ -57,37 +66,224 @@ class Form extends Component{
 
         axios.get(`https://cors-anywhere.herokuapp.com/http://www.app.educationspots.com/api/search/v3/?oAuth=0c74b7f78409a4022a2c4c5a5ca3ee19&&firstname=${this.state.firstName}&lastname=${this.state.lastName}&address=123+Maine+St&zip=${this.state.zip}&city=${this.state.city.toUpperCase()}&state=${this.state.state.toUpperCase()}&email=billstone1ccc%40gmail.com&homephone=${this.state.phone}&programid=${this.state.school}&concentrationid=${this.state.program}&hsgradyear=${this.state.year}&educationlevel=${this.state.level}`)
           .then(response => {
-                // console.log("From the response", response.data)
                 this.setState({
-                    data: response.data.SEARCHRESULTS.COLLEGE
+                    data: response.data.SEARCHRESULTS,
+                    image: response.data.SEARCHRESULTS.COLLEGE[0]
+                    .LOGO,
+                    collegeName: response.data.SEARCHRESULTS.COLLEGE[0].NAME,
+                    programInterest: response.data.SEARCHRESULTS.COLLEGE[0].FIELDPARAMETER[0].FIELDVALUES,
+                    rnLicense: response.data.SEARCHRESULTS.COLLEGE[0].FIELDPARAMETER[1].FIELDVALUES,
+                    usCitizen: response.data.SEARCHRESULTS.COLLEGE[0].FIELDPARAMETER[2].FIELDVALUES,
+                    leadStatus: response.data.SEARCHRESULTS.COLLEGE[0].FIELDPARAMETER[3].FIELDVALUES,
+                    compAccess: response.data.SEARCHRESULTS.COLLEGE[0].FIELDPARAMETER[4].FIELDVALUES,
+                    isLoaded: true,
                 })
-                console.log("from the state", this.state.data)
             })
-          .catch(error => {console.log(error)});
+          .catch(error => {
+              this.setState({
+                  error,
+              })
+          });
 
         this.setState({
-          firstName: "",
-          lastName: "",
-          address: "",
-          zip: "",
-          city: "",
-          state: "",
-          email: "",
-          phone: "",
-          program: "",
-          school: "",
-          year: "",
-          level: "",
-          data: [],
+            firstName: "",
+            lastName: "",
+            address: "",
+            zip: "",
+            city: "",
+            state: "",
+            email: "",
+            phone: "",
+            program: "",
+            school: "",
+            year: "",
+            level: "",
+            data: "",
+            image: "",
+            programInterest: [],
+            rnLicense: [],
+            leadStatus: [],
+            compAccess: [],
+            usCitizen: [],
+            isLoaded: false,
+            error: null,
         })
-        console.log(this.state);
       }
 
       handleRender = () => {
+        const programList = this.state.programInterest.map(label => {
+            return (
+                <option
+                    key={label.FVALUE}
+                >
+                    {label.FLABEL}
+                </option>
+            )
+        })
+
+        const rnLic = this.state.rnLicense.map(license => {
+            return (
+                <option
+                    key={license.VALUE}
+                >
+                    {license.FLABEL}
+                </option>
+            )
+        })
+
+        const lead = this.state.leadStatus.map(l => {
+            return (
+                <option
+                    key={l.VALUE}
+                >
+                    {l.FLABEL}
+                </option>
+            )
+        })
+
+        const citizen = this.state.usCitizen.map(us => {
+            return (
+                <option
+                    key={us.VALUE}
+                >
+                    {us.FLABEL}
+                </option>
+            )
+        })
+
+        const computer = this.state.compAccess.map(comp => {
+            return (
+                <option
+                    key={comp.VALUE}
+                >
+                    {comp.FLABEL}
+                </option>
+            )
+        })
+
         return (
-            <h1>
-                {/*{this.state.data.SEARCHRESULTS}*/}
-            </h1>
+            <div
+                style={{
+                    flex: 1,
+                    margin: "0 auto",
+                }}
+            >
+                <h1>
+                    {this.state.collegeName}
+                </h1>
+                {this.state.isLoaded ?
+                    <img 
+                    style={{
+                        width: 200,
+                        hight: 80
+                    }}
+                    src={this.state.image}
+                    alt="College"
+                    />
+                    :
+                    ""
+                }
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <label>
+                        Program of Interest
+                    </label>
+                    <select
+                        style={{
+                            width: 300,
+                        }}
+                    >
+                        <option>
+                            --- Select Option ---
+                        </option>
+                        {this.state.isLoaded ? programList : ""}
+                    </select>
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <label>
+                        Do you have a RN License?
+                    </label>
+                    <select
+                        style={{
+                            width: 300,
+                        }}
+                    >
+                        <option>
+                            --- Select Option ---
+                        </option>
+                        {this.state.isLoaded ? rnLic : ""}
+                    </select>
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <label>
+                        Lead Status
+                    </label>
+                    <select
+                        style={{
+                            width: 300,
+                        }}
+                    >
+                        <option>
+                            --- Select Option ---
+                        </option>
+                        {this.state.isLoaded ? lead : ""}
+                    </select>
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <label>
+                        Are you US Citizen?
+                    </label>
+                    <select
+                        style={{
+                            width: 300,
+                        }}
+                    >
+                        <option>
+                            --- Select Option ---
+                        </option>
+                        {this.state.isLoaded ? citizen : ""}
+                    </select>
+                </div>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}
+                >
+                    <label>
+                        Computer Access
+                    </label>
+                    <select
+                        style={{
+                            width: 300,
+                        }}
+                    >
+                        <option>
+                            --- Select Option ---
+                        </option>
+                        {this.state.isLoaded ? computer : ""}
+                    </select>
+                </div>
+            </div>
         )
       }
 
@@ -157,7 +353,12 @@ class Form extends Component{
                     >
                         Results:
                     </h1>
-                    <div>
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                        }}
+                    >
                         {this.handleRender()}
                     </div>
             </div>
